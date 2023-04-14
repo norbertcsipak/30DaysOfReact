0e6c50cc2290930a8358c380ab1a20fad80e8386
Norbert Csipak <norbertcipak@me.com>
2023-04-14 11:53:20 +0100

    arrays in JS, all arrays have the same data type in this example

---
+++ JavaScriptRefresher/Variables/arrays.js
@@ -0,0 +1,25 @@
+// How to create an array in JS
+const numbers = [0, 3.14, 4, 5.98, 6] //array of numbers
+const fruits = ['banana', 'apple', 'orange'] //array of strings, fruits
+const vegetables = ['carrot', 'potato', 'tomato', 'onion'] //array of strings, vegetables
+const animalProducts = ['milk', 'meat', 'butter', 'yoghurt'] //array of strings, animalProducts
+const webTechs = ['HTML', 'CSS', 'React', 'JS', 'Redux', 'Node', 'MongDB'] //array of strings, webTechs
+const countries = ['Serbia', 'Hungary', 'United Kingdom', 'Sweden', 'Denmark', 'Finland'] //array of strings, countries
+
+console.log('Numbers:', numbers);
+console.log('Number of numbers:', numbers.length);
+
+console.log('Fruits:', fruits);
+console.log('Number of fruits:', fruits.length);
+
+console.log('Vegetables:', vegetables);
+console.log('Number of vegetables:', vegetables.length);
+
+console.log('Animal products:', animalProducts);
+console.log('Number of animal products:', animalProducts.length);
+
+console.log('Web technologies:', webTechs);
+console.log('Number of web technologies:', webTechs.length);
+
+console.log('Countries:', countries);
+console.log('Number of countries:', countries.length);
