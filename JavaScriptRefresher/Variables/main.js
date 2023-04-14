aece1a7aaa60021565bc4e203bea6f38a78af879
Norbert Csipak <norbertcipak@me.com>
2023-04-14 11:29:25 +0100

    Declaration of variables in JS

---
+++ JavaScriptRefresher/Variables/main.js
@@ -0,0 +1,13 @@
+// To declare variables in JS we use const for variables that do not change their value and
+// let for variables that change their values
+
+let firstName = 'Peter';
+firstName = 'George';
+
+// In this case the value of firstName is reassigned.
+// When I declared variable firstName it had a value of Peter
+// After reassigning to Gerorge, the value of the variable is now George
+
+const PI = 3.14;
+
+// If I try to reassign the value of PI, JS will throw an error
